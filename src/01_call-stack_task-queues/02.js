c60ddaf734dbc38991cf4058f605ee964196df78
Arijit <arijitdasofficial@gmail.com>
2025-01-19 16:24:17 +0530

    feat: init

--- src/01_call-stack_task-queues/02.js
+++ src/01_call-stack_task-queues/02.js
@@ -1,8 +1,5 @@
 console.log('Hi 0');
 
-const res = await Promise.resolve('Hi 1');
-console.log(res);
-
 setTimeout(function settimeout() {
   console.log('Hi 2');
 }, 0);
@@ -15,14 +12,17 @@ Promise.resolve('Hi 4').then(function promise_hi_4(resolve) {
   console.log(resolve);
 });
 
+const res = await Promise.resolve('Hi 1');
+console.log(res);
+
 console.log('Hi 5');
 
 /* 
 Hi 0
-Hi 1
-Hi 5
 Hi 3
 Hi 4
+Hi 1
+Hi 5
 Hi 2
 */
 
@@ -33,22 +33,22 @@ Code Execution Breakdown:
   > This is a synchronous log and executes immediately.
   > Logs: Hi 0
 
-2. await Promise.resolve('Hi 1'):
+2. setTimeout(function settimeout() { ... }, 0):
+  > The setTimeout callback is scheduled and placed into the macrotask queue to execute, after the synchronous code and microtasks have finished.
+
+3. Promise.resolve('Hi 3'):
+  > This creates a promise that resolves immediately.
+  > The .then callback (promise_hi_3) is added to the microtask queue.
+  
+4.Promise.resolve('Hi 4):
+  > Similarly, this creates another promise that resolves immediately.
+  > Its .then callback (promise_hi_4) is added to the microtask queue, after promise_hi_3.
+
+5. await Promise.resolve('Hi 1'):
   > The await pauses the execution of the rest of the main function until the promise resolves.
   > While the function is paused, other queued tasks (microtasks and synchronous code) may execute.
   > The promise resolves immediately with the value "Hi 1".
   > When the promise resolves, the function continues, and the value "Hi 1" is assigned to res.
-
-3. setTimeout(function settimeout() { ... }, 0):
-  > The setTimeout callback is scheduled and placed into the macrotask queue to execute, after the synchronous code and microtasks have finished.
-
-4. Promise.resolve('Hi 3'):
-  > This creates a promise that resolves immediately.
-  > The .then callback (promise_hi_3) is added to the microtask queue.
-  
-5.Promise.resolve('Hi 4):
-  > Similarly, this creates another promise that resolves immediately.
-  > 4. Promise.resolve('Hi 3'):
 
 6. console.log('Hi 5'):
   > This is a synchronous log, so it executes immediately.
